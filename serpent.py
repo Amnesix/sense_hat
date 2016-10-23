cdb10edac8b24c0c2be3a4f7508b8f749bace598
Amnesix <jm.technique@free.fr>
2016-10-23 19:08:01 +0200

    Dernière améliorations

--- serpent.py
+++ serpent.py
@@ -14,11 +14,20 @@ from signal import pause
 from random import randint
 from threading import Timer
 
-t = (64, 64, 64)
-s = (0, 64, 0)
-n = (0, 0, 64)
-p = (64, 0, 0)
-o = (96,48,0)
+rouge = (96, 0, 0)
+rouge_fonce = (48, 0, 0)
+vert = (0, 96, 0)
+vert_fonce = (0, 48, 0)
+bleu = (0, 0, 96)
+bleu_fonce = (0, 0, 96)
+jaune = (96, 96, 0)
+jaune_fonce = (48, 48, 0)
+orange = (96, 48, 0)
+majenta = (96, 0, 96)
+cyan = (0, 96, 96)
+blanc = (96, 96, 96)
+noir = (0, 0, 0)
+
 
 def clamp(val, min_val=0, max_val=7):
     return min(max_val, max(min_val, val))
@@ -39,7 +48,7 @@ class Jeu:
         self.sense.stick.direction_middle = self.push_button
         self.dx = self.dy = 0
         self.en_cours = False
-        self.sense.clear(o)
+        self.sense.clear(orange)
         Timer(1, self.clear).start()
 
     def clear(self):
@@ -73,8 +82,9 @@ class Jeu:
             self.serpent = [(self.x, self.y)]
             self.dx = self.dy = 0
             self.duree = .5
-            self.sense.set_pixel(self.x, self.y, (0, 64, 0))
+            self.sense.set_pixel(self.x, self.y, vert)
             self.bouffe = False
+            self.score = 0
             self.run()
 
     def run(self):
@@ -88,23 +98,40 @@ class Jeu:
         self.sense.clear()
         self.x = rotate(self.x+self.dx)
         self.y = rotate(self.y+self.dy)
-        if (self.x, self.y) in self.serpent and self.dx + self.dy != 0:
-            self.en_cours = False
-            self.sense.show_message("SCORE %d"%len(self.serpent), 0.05,
-                               text_colour=(64, 64, 0), back_colour=(64, 0, 0))
-            Timer(1, self.clear).start()
-            return
+        if self.dx + self.dy != 0:
+            if (self.x, self.y) in self.serpent:
+                self.en_cours = False
+                self.sense.show_message("SCORE %d"%self.score, 0.05,
+                                        text_colour=jaune,
+                                        back_colour=rouge_fonce)
+                Timer(1, self.clear).start()
+                return
+            self.duree*=.995
+            if self.duree < .1:
+                self.duree = .1
         self.serpent.append((self.x, self.y))
         if self.x == self.nx and self.y == self.ny:
             self.bouffe = False
+            self.score += 1
+            if len(self.serpent) >= 20:
+                self.serpent.pop(0)
         else:
             self.serpent.pop(0)
-        self.sense.set_pixel(self.nx, self.ny, n)
-        if len(self.serpent) > 1:
-            for i in self.serpent[:-1]:
-                self.sense.set_pixel(i[0], i[1], s)
-        self.sense.set_pixel(self.x, self.y, t)
-        self.duree*=.995
+        self.sense.set_pixel(self.nx, self.ny, bleu)
+        j = 0
+        for i in range(len(self.serpent)-1, -1, -1):
+            c = self.serpent[i]
+            if j == 0:
+                self.sense.set_pixel(c[0], c[1], vert)
+            elif j%3 == 0:
+                self.sense.set_pixel(c[0], c[1], vert_fonce)
+            else:
+                self.sense.set_pixel(c[0], c[1], jaune_fonce)
+            j += 1
+        #if len(self.serpent) > 1:
+            #for i in self.serpent[:-1]:
+                #self.sense.set_pixel(i[0], i[1], vert_fonce)
+        #self.sense.set_pixel(self.x, self.y, vert)
         Timer(self.duree, self.run).start()
 
 
@@ -114,3 +141,5 @@ try:
     pause()
 except KeyboardInterrupt:
     print("Terminé !")
+sleep(.5)
+SenseHat().clear()
